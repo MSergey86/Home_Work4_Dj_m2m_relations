dd89fd65ddc334008474ee91781b627f093440ee
MSergey86 <prdx.morozov@mail.ru>
2022-11-11 23:22:38 +0300

    first commit

---
+++ articles/admin.py
@@ -0,0 +1,33 @@
+from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
+
+from .models import Article, Scope, Tag
+
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        count = 0
+        for form in self.forms:
+            if form.cleaned_data.get('is_main') == True:
+                count += 1
+            if count > 1:
+                raise ValidationError('Должен быть только один главный тэг')
+        if count == 0:
+            raise ValidationError('Укажите главный тэг')
+        return super().clean()
+
+class ScopeInline(admin.TabularInline):
+    model = Scope
+    extra = 2
+    formset = ScopeInlineFormset
+
+@admin.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ['title', 'text', 'published_at', 'image']
+    list_filter = ['published_at']
+    inlines = [ScopeInline]
+
+@admin.register(Tag)
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['id', 'name']
+    # inlines = [ScopeInline]
